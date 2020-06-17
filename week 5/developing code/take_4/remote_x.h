0e0608f3f878510fb9616bca3198262f11ead1c4
Prit Varmora <56828284+PRITVARMORA@users.noreply.github.com>
2020-06-18 00:59:14 +0530

    using servotimer2 librery to avoid collision of timmers
    all ISR vectors are perfect
    registr configuration is also perfect
    ISR of timer1 is  not conflicting with ISR of Servo (solved issue)
    
    unsolved issues :x:
    the servo is behaving wiard while running ISR B with datafusion call
    
    all ISR gets crashed while i uncomments ISR C  because of interaction with MPU 6050(nay be because it uses ISR too)

---
+++ week 5/developing code/take_4/remote_x.h
@@ -0,0 +1,58 @@
+#include<arduino.h>
+
+void read_switch();
+int jx = 500,jy = 500;
+int backwheel, turnwheel ;
+int angle_0 = 108;
+
+void Adjust_rmt_Data();
+
+void read_remote()
+{
+    if (Serial1.available()>16 )
+    {    //Serial.println(Serial1.read());
+        if (Serial1.read()==0x7e)
+        {
+        for (int i =0 ; i<10; i ++)
+        {   
+            Serial1.read();
+        }
+          jx = (Serial1.read()<<8) | Serial1.read();
+          jy = (Serial1.read()<<8) | Serial1.read();
+          Serial1.read();
+          Adjust_rmt_Data();
+    }
+}
+}
+
+void Adjust_rmt_Data()
+{
+
+//-------------------------------xaxis------------------------- forward / backward /stop  
+if (jx < 470){
+  
+    backwheel = map(jx, 470, 0, 0, -254);  //backward
+
+}
+
+else if (jx > 550) {
+    backwheel = map(jx, 550, 1023, 0, 254); // forward
+}
+
+else if (jx <550 && jx >470) {
+    backwheel = 0;                          //stop
+  }
+//---------------------------yaxis----------------------------- turn/ left/right
+if (jy < 400) {
+    turnwheel = map(jy, 400, 0, angle_0, angle_0+60);;                         //left
+  }
+  
+else if (jy > 550) {
+    turnwheel = map(jy, 550, 1023, angle_0, angle_0-65);                         // right
+}
+
+else if (jy <550 && jy >470) {
+    turnwheel = angle_0;                         //straight 
+  }
+
+}
---
+++ week 5/developing code/take_4/take_4.ino
@@ -0,0 +1,90 @@
+#include "remote_x.h"
+#include "motor_run.h"
+#include "mpu_header.h"
+//######################################################---------------------------------------------declarations
+
+float Kp = -120.80820362847, Ki = 0, Kd = 0;
+//float Kp = -27014.7125538481, Ki = -21183.1159387893, Kd = -5137.85556983136;-6.98870101025249
+//float Kp = -27000, Ki = 0, Kd = -5137.85556983136;
+//######################################################---------------------------------------------declarations
+
+float set_point = 2.32;
+float error= 0, error_d = 0,  error_i = 0 , prev_error = 0;
+float Rmotor_torque ;
+float bot_inclination;
+void PID_controle();
+//######################################################---------------------------------------------SETUP
+
+void setup() {
+Serial1.begin(115200);
+Serial.begin(250000);
+Wire.begin();
+Wire.setClock(400000UL);
+            
+Adjust_rmt_Data();
+resetMPU();
+cli();               //to reinitialize all interupts 
+TCCR1A = B00000000;  //clear whole register  
+TCCR1A = B00001101;  //setting up prescaler to 1024 and ctc mode too
+
+OCR1AH = 0x01;       //high byte of compare match register
+OCR1AL = 0x39;       //low byte of compare match register//interupt after 20ms
+
+OCR1BH = 0x01;       //high byte of compare match register
+OCR1BL = 0x19;       //low byte of compare match register//interrupt after 18ms
+
+OCR1CH = 0x00;       //high byte of compare match register
+OCR1CL = 0xF9;       //low byte of compare match register//intrrupt after 16ms
+
+TIMSK1 = B00001110;  //enabling timer interrupt on compare match register A 
+sei(); 
+}
+
+//######################################################-------------------------------------------LOOP
+void loop() {
+read_remote();
+run_backwheel();
+run_front_servo();
+//######################################################
+//Serial.print(" ------- ");
+//Serial.print(accelYangle);
+//Serial.print(" ------- ");
+//Serial.print(gyroYang);
+//Serial.print(" ------- ");
+//Serial.print((Kd* error_d));
+//Serial.print(" ------- ");
+//Serial.print((Ki * error_i));
+//Serial.print(" ------- ");
+//Serial.print((Kp * error));
+//Serial.print(" ------- ");
+//Serial.println(pitch);
+//######################################################
+}
+
+ISR(TIMER1_COMPA_vect)     //writing ISR with vector for timer 1A  // run pid calculation after 20ms it willl take 200 micro sec time         
+{
+  PID_controle();
+  }
+ISR(TIMER1_COMPB_vect)     //writing ISR with vector for timer 1B  //run data fusion after 19 ms it will take 400 micro sec time            
+{ 
+//  bot_inclination = dataFusion();
+  }
+ISR(TIMER1_COMPC_vect)     //writing ISR with vector for timer 1C  // grabing data at 18 ms it takes around 700 micro sec          
+{
+//  readAccel(16384.0);                                                //read XYZ Accel data from registers 0x3B to 0x40 
+//  readGyro(32.75); 
+  }
+
+//######################################################---------------------------------------------PID controler here
+void PID_controle()
+{
+error = set_point - bot_inclination;
+error_i = error_i + error * Ts;
+error_d = (error - prev_error)/Ts;
+prev_error = error;
+
+Rmotor_torque = (Kp * error) + (Ki * error_i)+ (Kd* error_d);
+run_Rwheel(Rmotor_torque);
+
+}
+
