ac435a2d76a260e845d6cd29ca5e514342f5811f
Prit Varmora <56828284+PRITVARMORA@users.noreply.github.com>
2020-06-21 00:04:53 +0530

    Update take_2.ino

--- week 5-6/developing code/take_2/take_2.ino
+++ week 5-6/developing code/take_2/take_2.ino
@@ -4,9 +4,9 @@
 //######################################################-------------------------------------------declarations
 
 //float Kp = -180, Ki = -100, Kd = -0.5;
-float Kp = -90, Ki = -70, Kd = -1;//-0.6;
-//float Kp = -27014.7125538481, Ki = -21183.1159387893, Kd = -5137.85556983136;
-//float Kp = -27000, Ki = 0, Kd = -5137.85556983136;
+//float Kp = -90, Ki = -70, Kd = -1;
+float Kp = -180, Ki = 0, Kd = 0;
+
 //######################################################---------------------------------------------declarations
 
 float set_point = -3.32;
@@ -27,14 +27,18 @@ void setup() {
 //######################################################-------------------------------------------LOOP
 
 void loop() {
+if (Serial.available())
+{
+  Kp = Kp - float(Serial.read())+38;
+  Serial.println(Kp);
+}
+  
   //timer1 = micros();
   read_remote();
   run_backwheel();
   run_front_servo();
-  //Serial.println((micros()- timer1));
 
   float bot_inclination = dataFusion();
-  //Serial.println((micros()- timer1));
   //######################################################---------------------------------------------PID controler here
 
   error = set_point - bot_inclination;
@@ -57,18 +61,18 @@ void loop() {
   run_Rwheel(Rmotor_torque);
 
   //######################################################
-  Serial.print(" ------- ");
-  Serial.print(255);
-  Serial.print(" ------- ");
-  Serial.print(-255);
-  Serial.print(" ------- ");
-  Serial.print((Kp * error));
-  Serial.print(" ------- ");
-  Serial.print((Ki * error_i));
-  Serial.print(" ------- ");
-  Serial.print((Kd* error_d));
-  Serial.print(" ------- ");
-  Serial.println(error);
+//  Serial.print(" ------- ");
+//  Serial.print(255);
+//  Serial.print(" ------- ");
+//  Serial.print(-255);
+//  Serial.print(" ------- ");
+//  Serial.print((Kp * error));
+//  Serial.print(" ------- ");
+//  Serial.print((Ki * error_i));
+//  Serial.print(" ------- ");
+//  Serial.print((Kd* error_d));
+//  Serial.print(" ------- ");
+//  Serial.println(error);
   //######################################################
 
   while (micros() - timer < Ts * 1000000); ////minimum time gap remains to burn is 1.2 ms so we can afford Fs = 200 Hz ~ Ts = 5 ms is optimum
